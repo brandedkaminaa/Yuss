a9524192ebf833f54e53b5decadcefd14b340db6
brandedkaminaa <78950515+brandedkaminaa@users.noreply.github.com>
2024-09-30 15:32:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-<script src="firebaseConfig.js"></script> 
-  <script> 
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCphoH9_NPxSzgnh6kjXvPTlQdHOGkFGgQ",
+  authDomain: "simple-project-de502.firebaseapp.com",
+  databaseURL: "https://simple-project-de502-default-rtdb.firebaseio.com",
+  projectId: "simple-project-de502",
+  storageBucket: "simple-project-de502.appspot.com",
+  messagingSenderId: "194813539682",
+  appId: "1:194813539682:web:f5260ae96cde439c0f683b",
+  measurementId: "G-HG982T31BL"
+};
+
   // Initialize Firebase 
-  firebase.initializeApp(firebaseConfig); 
+firebase.initializeApp(firebaseConfig); 
 const db = firebase.database(); 
 // Your existing Firebase logic here... 
 
@@ -165,4 +175,3 @@ document.addEventListener('DOMContentLoaded', function() {
     document.removeEventListener('touchend', stopDrag);
   }
 });
-</script>
